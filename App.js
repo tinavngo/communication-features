6d2ae970d9264d3eb3e64c8cf8f0b3bca2202c1e
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-07-01 17:56:57 -0700

    Add getLocation
    users can access their location from Google Maps
    1. Permission is requested
    2. Display location on map once permission is granted

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import * as Location from 'expo-location';
 
 export default function App() {
   const [image, setImage] = useState(null);
+  const [location, setLocation] = useState(null);
 
   // Button #1 Pick an image
   const pickImage = async () => {
@@ -30,12 +31,24 @@ export default function App() {
       let result = await ImagePicker.launchCameraAsync();
 
       if(!result.canceled) {
-        let MediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
+        let mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
 
-        if (MediaLibraryPermissions?.granted) await MediaLibrary.saveToLibraryAsync(result.assets[0].uri);
+        if (mediaLibraryPermissions?.granted) await MediaLibrary.saveToLibraryAsync(result.assets[0].uri);
 
         setImage(result.assets[0]);
       } else setImage(null)
+    }
+  }
+
+  // Button #3 Get location
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+
+    if(permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } else {
+      Alert.alert("Permissions to read location aren't granted");
     }
   }
 
@@ -51,6 +64,21 @@ export default function App() {
       <Button
       title="Take a photo"
       onPress={takePhoto}
+      />
+      {location &&
+      <MapView 
+      style={{width: 300, height: 200}}
+      region={{
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+      />
+      }
+      <Button
+      title="Get my location"
+      onPress={getLocation}
       />
       <StatusBar style="auto" />
     </View>
